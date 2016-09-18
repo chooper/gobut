ae6192d52492d69b82c2350ec1ec652abdb1ae9b
Charles Hooper <chooper@plumata.com>
2016-09-18 09:25:04 -0700

    Fix builds so docker containers are super small (~2MB)

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -eux -o pipefail
 
 test -z "$1" && { echo "Usage: $0 <version tag>"; exit 1; }
-$tag = $1
+readonly tag=$1
 
-docker build -t chooper/gobut:$tag .
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gobut .
+docker build -t chooper/gobut:$tag -f Dockerfile.scratch .
+
+echo "Publish using \`docker push chooper/gobut\`"
