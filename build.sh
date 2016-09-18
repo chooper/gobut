df69ea91414ff361af30848078cf7d458eb200b2
Charles Hooper <chooper@plumata.com>
2016-09-18 09:39:14 -0700

    Bundle CA certs into builds

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -eux -o pipefail
 test -z "$1" && { echo "Usage: $0 <version tag>"; exit 1; }
 readonly tag=$1
 
+curl --remote-name --time-cond cacert.pem https://curl.haxx.se/ca/cacert.pem
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o gobut .
 docker build -t chooper/gobut:$tag -f Dockerfile.scratch .
 
